324de80ecc426e6a67b737b69c03bf73a0acd329
Ruqin Zhou <zhouruqin@outlook.com>
2023-05-09 23:58:53 +0800

    test

--- src/main/java/net/chow/Main.java
+++ src/main/java/net/chow/Main.java
@@ -1,6 +1,5 @@
 package net.chow;
 
-import net.chow.pulsar.MyPulsarSource;
 import net.chow.task.CTe1;
 import net.chow.task.TFactory;
 
@@ -9,35 +8,11 @@ import java.util.concurrent.*;
 public class Main {
     public static void main(String []args) {
         ThreadPoolExecutor exec = new ThreadPoolExecutor(
-                1, 1, 0, TimeUnit.SECONDS,
-                new LinkedBlockingDeque<>(1), new TFactory());
-        // 如果supplyAsync在调用thenAccept前就complete，则thenAccept会阻塞
-        // 反正不会
-        // 即使用thenAccept时，前面步骤过快则会阻塞在thenAccept处
-        CompletableFuture.supplyAsync(() -> {
-//            try {
-//                Thread.sleep(1000);
-//            } catch (InterruptedException e) {
-//                throw new RuntimeException(e);
-//            }
-            System.out.println(1);
-            return 1;
-        }, exec).thenAcceptAsync(e -> {
-            try {
-                Thread.sleep(2000);
-            } catch (InterruptedException ex) {
-                throw new RuntimeException(ex);
-            }
-            System.out.println(e + 1);
-        }, exec);
-
-        System.out.println("out");
-        try {
-            Thread.sleep(2000);
-        } catch (Exception ex) {
-            ex.printStackTrace();
-        }
-        exec.shutdown();
-        System.out.println("exit");
+                10, 10, 0, TimeUnit.SECONDS,
+                new LinkedBlockingDeque<>(1), new ThreadPoolExecutor.AbortPolicy());
+        long start = System.currentTimeMillis();
+        ScheduledThreadPoolExecutor executor = new ScheduledThreadPoolExecutor(1);
+        CTe1 t = new CTe1(exec, start);
+        executor.scheduleWithFixedDelay(t, 0, 1, TimeUnit.SECONDS);
     }
 }
--- src/main/java/net/chow/task/CTe1.java
+++ src/main/java/net/chow/task/CTe1.java
@@ -1,17 +1,49 @@
 package net.chow.task;
 
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ThreadPoolExecutor;
+
 public class CTe1 extends Thread {
 
-    private int count;
-    public CTe1() {
+    private final ThreadPoolExecutor exec;
+    private final long start;
+    public CTe1(ThreadPoolExecutor exec, long start) {
         super(CTe1.class.getName());
-        count = 0;
+        this.exec = exec;
+        this.start = start;
     }
 
     @Override
     public void run() {
-        System.out.println(count);
-        count++;
-        throw new NoClassDefFoundError();
+        try {
+            long cur = System.currentTimeMillis();
+            System.out.printf("Margin: %d%n", cur - start);
+
+            CompletableFuture<Integer> ret = CompletableFuture.supplyAsync(() -> {
+//            try {
+//                Thread.sleep(1000);
+//            } catch (InterruptedException e) {
+//                throw new RuntimeException(e);
+//            }
+                System.out.println(1);
+                return 1;
+            }, exec);
+            Thread.sleep(10);
+
+            // 如果supplyAsync在调用thenAccept前就complete，则thenAccept会阻塞
+            // 反正不会
+            // 即使用thenAccept时，前面步骤过快则会阻塞在thenAccept处
+            // 但只要不阻塞，都不会影响外层scheduleWithFixedDelay的调用频率
+            ret.thenAcceptAsync(e -> {
+                try {
+                    Thread.sleep(2000);
+                } catch (InterruptedException ex) {
+                    throw new RuntimeException(ex);
+                }
+                System.out.println(e + 1);
+            }, exec);
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
     }
 }
